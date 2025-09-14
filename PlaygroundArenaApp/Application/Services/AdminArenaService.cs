384bf6c791a2e8f6fb6140a3532d10fc6a8cf04c
abalex-ui <kibgheeo775533@gmail.com>
2025-09-14 05:22:10 +0500

    AddUser AddArena Endpoint Created

--- PlaygroundArenaApp/Application/Services/AdminArenaService.cs
+++ PlaygroundArenaApp/Application/Services/AdminArenaService.cs
@@ -1,4 +1,9 @@
-﻿using PlaygroundArenaApp.Infrastructure.Data;
+﻿using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.EntityFrameworkCore;
+using PlaygroundArenaApp.Core.DTO;
+using PlaygroundArenaApp.Core.Models;
+using PlaygroundArenaApp.Infrastructure.Data;
+using System.Web.Mvc;
 
 namespace PlaygroundArenaApp.Application.Services
 {
@@ -17,6 +22,51 @@ namespace PlaygroundArenaApp.Application.Services
 
         //All write/add services
 
+
+        public async Task<bool> CreateArenaService( AddArenaDTO dto )
+        {
+            // I allowed same emails for multiple arenas 
+            //so that If someone has arenas in multiple cities
+            //he can manage all of them with single email
+
+            var arena = new Arena
+            {
+                Name = dto.Name,
+                Email = dto.Email,
+                Location = dto.Location
+            };
+
+            await _context.Arenas.AddAsync(arena);
+
+            await _context.SaveChangesAsync();
+
+            return true;
+        }
+
+
+        public async Task<bool> CreateUserService( AddUserDTO dto)
+        {
+            //User cannot have same Email or same Phone Numbers
+            var checkEmail = await _context.Users.FirstOrDefaultAsync(u => u.Email == dto.Email);
+            if (checkEmail != null)
+                throw new BadHttpRequestException("Email already in use!");
+
+            var checkPhone = await _context.Users.FirstOrDefaultAsync(u => u.PhoneNumber == dto.PhoneNumber);
+            if (checkPhone != null)
+                throw new BadHttpRequestException("Phone Number already in use!");
+
+            var user = new User
+            {
+                Name = dto.Name,
+                PhoneNumber = dto.PhoneNumber,
+                Email = dto.Email
+            };
+
+            await _context.Users.AddAsync(user);
+            await _context.SaveChangesAsync();
+
+            return true;
+        }
 
     }
 }
